c9cd40f2ec757fac5b5b3aba0b4cc15f09283955
GabielFemi <gabrielfemi799@gmail.com>
2020-05-09 20:02:07 +0200

    add start server function

---
+++ gabbymux.go
@@ -0,0 +1,11 @@
+package gabbymux
+
+
+import (
+	_"fmt"
+)
+
+func Router() {
+	// This is the default router for Gabbymux
+}
+
---
+++ start_server.go
@@ -0,0 +1,10 @@
+package gabbymux
+
+
+import (
+	"net/http"
+)
+
+func start(w http.ResponseWriter, r *http.Request, port string) {
+	// Start the server on the specified port.
+}
